de5246e448d6fbd77c1ffe8106bb91f788a1e4cd
u1and0 <e01.ando60@gmail.com>
2022-10-11 12:42:26 +0900

    [mod] string to seq[string] and join "&"

--- jtr.nim
+++ jtr.nim
@@ -26,11 +26,9 @@ import json, strformat, strutils
 
 proc objectTree*(jobj: JsonNode, indent: string = ""): string =
   var nextIndent: string
-  const defaultIndent: string = " ".repeat(4)
-  var res: string
+  var res: seq[string]
   var branch: string = "├── "
   for k, v in jobj.pairs:
-    # echo &"key: {k}, val: {$v}"
     let types: string = case v.kind
       of JNull: "<null>"
       of JString: "<string>"
@@ -39,16 +37,19 @@ proc objectTree*(jobj: JsonNode, indent: string = ""): string =
       of JInt: "<int>"
       of JArray: "<array>"
       of JObject: "\n" & objectTree(v, nextIndent)
-    res &= &"{indent}{branch}{k} {types}\n"
-    nextIndent = indent & defaultIndent
-  return res
+    res.add(&"{indent}{branch}{k} {types}")
+    nextIndent = indent & " ".repeat(4)
+  return res.join("\n")
 
 when isMainModule:
   let line = stdin.readLine
   let jobj = line.parseJson()
   let jstring = jobj.pretty()
-  echo jstring # jqと同じように、JSONを解釈してstdoutへ表示する
 
   echo "."
   echo objectTree(jobj)
 
+  # TODO
+  # 後でオプションで表示切替
+  # デフォルト非表示
+  echo jstring # jqと同じように、JSONを解釈してstdoutへ表示する
--- tests/testjtr.nim
+++ tests/testjtr.nim
@@ -9,11 +9,12 @@ discard """
 ├── obj
     ├── bar <int>
     ├── baz <string>
-
 ├── name <string>
 
 '''
 """
 
-const line = """{"foo": "0", "obj": {"bar":1, "baz":"2"}, "name": "ken"}"""
+const line = """
+{"foo": "0", "obj": {"bar":1, "baz":"2"}, "name": "ken"}
+"""
 echo objectTree(line.parseJson())
